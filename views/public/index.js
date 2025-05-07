8c1a64fcd84057a551d4f938edfad490b4f13602
NextGithub90 <akunbeta958@gmail.com>
2025-05-07 19:06:22 +0700

    ...

--- views/public/index.js
+++ views/public/index.js
@@ -6,7 +6,7 @@ const { loadDetails, findDetail } = require("./detail.js");
 // Template engine (gunakan ejs)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../")); // Atur folder views ke "c:\Users\Lenovo\portofolio-5\views"
-console.log("Views folder:", path.join(__dirname, "../pages"));
+// console.log("Views folder:", path.join(__dirname, "../pages"));
 // app.use(express.json());
 
 // Middleware untuk melayani file statis dari folder "public"
