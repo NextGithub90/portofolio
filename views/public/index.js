6d91e7056a2572da464b155420d4554c0099d5f4
NextGithub90 <akunbeta958@gmail.com>
2025-05-11 20:38:21 +0700

    refactor: comment out urlencoded middleware for clarity

--- views/public/index.js
+++ views/public/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const path = require("path");
 const app = express();
 const port = 3000;
@@ -8,7 +9,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../")); //
 console.log("Views folder:", path.join(__dirname, "../pages"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 // Middleware untuk melayani file statis dari folder "public"
 const serveStatic = require("serve-static");
