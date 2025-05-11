12584cece45bc528da42341f3746016c378c70cd
NextGithub90 <akunbeta958@gmail.com>
2025-05-11 20:22:42 +0700

    refactor: update file reading method and uncomment server start

--- views/public/detail.js
+++ views/public/detail.js
@@ -3,7 +3,7 @@ const path = require("node:path");
 
 // Fungsi untuk memuat data kontak dari file JSON
 const loadDetails = () => {
-  const readFileSync = fs.readFileSync("../data/details.json", "utf8");
+  const readFileSync = fs.readFileSync(path.join(__dirname, "../data/details.json"), "utf8");
   const details = JSON.parse(readFileSync); // Renamed from 'contacks' to 'details'
   return details;
 };
